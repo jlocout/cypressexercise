232299c43baa05cb57bea92b651d15baa8b0a336
Jeremy Lokovic <jylokovic@gmail.com>
2020-05-11 15:25:19 -0600

    tutorial commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+    it('Does not do much! pass', () => {
+      expect(true).to.equal(true)
+    })
+    it('Does not do much! fail', () => {
+        expect(true).to.equal(false)
+      })
+  })
